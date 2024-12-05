0474f0a3f9584c2b67a45774c6de65d8a8c68ba1
niasekayi <nia.greene@bison.howard.edu>
2024-12-04 22:36:31 -0500

    Changed code to loop
    Fixed my code to a loop so that the code will continue to run and prompt the user to enter a number until a legal number is entered.

--- Project starting code.py
+++ Project starting code.py
@@ -1,3 +1,18 @@
 #COMP ORG PROJECT
 
+#testing loop for N, is it legal or naw
+
+while True:
+    try:
+        N = int(input("Enter a value for N:\n"))  
+
+        # Check if the input is legal
+        if N <= 0:
+            print("Illegal Number! ")
+        elif N < 25:
+            print("Illegal Number!")
+        else:
+            print("Number is legal!")
+            break  # stop the loop
+ 
 

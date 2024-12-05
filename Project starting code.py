603070a24a27697f5a60717556622d71ffd9a497
niasekayi <nia.greene@bison.howard.edu>
2024-12-04 22:30:42 -0500

    Starting by making the program in python
    I am starting by making my code in Python first and testing it there before translating to MIPS code

---
+++ Project starting code.py
@@ -0,0 +1,11 @@
+#COMP ORG PROJECT
+
+
+N= int(input("Enter a value for N:\n")) 
+    
+if N<=0:
+    print("Illegal Number! ")
+elif N < 25:
+    print("Illegal Number!")
+else:
+    print("Number is legal!")
